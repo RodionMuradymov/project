cde9737065803bb98d7230fe605286ff3ae7b2c0
Rodion <muradymov_rodion@mail.ru>
2021-09-17 12:53:35 +0300

    first commit

---
+++ Js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+// let number=4.6;
+// console.log(4/0);
+// console.log("hi"*9);
+// const persone='Alex';
+// const bool=true;
+
+// let und;
+// console.log(und);
+// const obj={
+//     name: "Jon",
+//     age: 25,
+//     isMarried: true
+// };
+// console.log(obj["age"]);
+// let arr=['a32','123','1231232'];
+// console.log(arr["0"]);
+// // alert('234234')
+// // const result = confirm("Are you here");
+// // console.log(result);
+// //ВСЯ ИНФОРМАЦИЯ ОТ ПОЛЬЗОВАТЕЛЕЙ БУДЕТ В ФОРМАТЕ СТРОКИ, МОЖНО ЭТО ИЗМЕНИТЬ
+// const anrw = prompt(" Вам есть 18?","");
+// console.log(anrw+65);
+// console.log(typeof(5));
+// const answer = [];
+// answer[0] = prompt('Hello','');
+// answer[1]=prompt("Как",'');
+// answer[2]=prompt("23",'');
+// document.write(answer);
+const uew=3224;
+console.log(`(${typeof(uew)})`);
+const шк = "Ничтожество";
+alert(`Привет, ${шк}`);
+

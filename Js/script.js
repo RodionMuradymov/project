dd5b68f98e285ad0d2965f6fe169184cc74d9dea
Rodion <muradymov_rodion@mail.ru>
2021-09-18 19:23:56 +0300

    543

--- Js/script.js
+++ Js/script.js
@@ -1,47 +1,19 @@
 "use strict";
-// let number=4.6;
-// console.log(4/0);
-// console.log("hi"*9);
-// const persone='Alex';
-// const bool=true;
+const numberOfFilms= prompt("Сколько фильмов вы уже посмотрели?");
+const personalMovieDB={
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat: false
 
-// let und;
-// console.log(und);
-// const obj={
-//     name: "Jon",
-//     age: 25,
-//     isMarried: true
-// };
-// console.log(obj["age"]);
-// let arr=['a32','123','1231232'];
-// console.log(arr["0"]);
-// // alert('234234')
-// // const result = confirm("Are you here");
-// // console.log(result);
-// //ВСЯ ИНФОРМАЦИЯ ОТ ПОЛЬЗОВАТЕЛЕЙ БУДЕТ В ФОРМАТЕ СТРОКИ, МОЖНО ЭТО ИЗМЕНИТЬ
-// const anrw = prompt(" Вам есть 18?","");
-// console.log(anrw+65);
-// console.log(typeof(5));
-// const answer = [];
-// answer[0] = prompt('Hello','');
-// answer[1]=prompt("Как",'');
-// answer[2]=prompt("23",'');
-// document.write(answer);
 
-console.log('arr '+  5);
-console.log('arr   '+"234");
-let incr= 10,
-decr=10;
-incr++;
---decr;
-console.log(--decr,incr++);
-console.log(5%2);
-console.log(incr);
-// && - и
-// || - или
-const checked=true,
-urt=true;
-console.log(checked&&!false);
+};
+const oneFilm=prompt("Один из последних просмотренных фильмов?");
+const ochenkf=prompt("На сколько оцените его");
+personalMovieDB.movies={onefilm:ochenkf};
+console.log(personalMovieDB);
+
 
 
 
